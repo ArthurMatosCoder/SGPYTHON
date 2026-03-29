c1fa81dce72b9795edc676380e1c1f1096d3ec33
ArthurMatosCoder <arthurmatos.studiogames@gmail.com>
2026-03-29 10:36:28 -0300

    Create atividade1revisao.py

---
+++ atividade1revisao.py
@@ -0,0 +1,33 @@
+#atividade de revisão 1
+
+AAAAAHHHHH = "eu sei fazer print assim"
+
+
+iiihhh = "e assim"
+
+
+print (AAAAAHHHHH + " Hello World")
+
+
+print (iiihhh + " Hello World")
+
+
+AAH_ = "20"
+
+
+iih_ = "6"
+
+
+print ( "a variavel AAAAAHHHHH " "tem " + AAH_ + " letras")
+
+
+print ( "a variavel iiihhh " "tem " + iih_ + " letras")
+
+
+ihh_porcento = 20 % 6
+
+
+print ("e uma variavel")
+
+
+print (ihh_porcento)
