458435f85ab0cccbe106a6c731126bd4ebb4f189
ArthurMatosCoder <arthurmatos.studiogames@gmail.com>
2026-03-29 10:35:29 -0300

    Create atividade2revisao.py

---
+++ atividade2revisao.py
@@ -0,0 +1,27 @@
+#atividade de revisão 2
+
+idade_arthur =  12
+
+
+texto = "minha idade "
+
+
+#print (texto + str(idade_arthur))
+
+
+altura_arthur = "1.70"
+
+
+idade_arthur += float(altura_arthur)
+
+
+#print ("e minha altura " + altura_arthur + " somada com minha altura resulta em " + str(idade_arthur))
+
+
+altura_igual = True
+
+
+idade_igual = False
+
+
+print ( "a altura  mudou " + str(altura_igual) + " e a idade mudou "+ str(idade_igual))
