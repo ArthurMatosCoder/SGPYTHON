f7a38fb07ae89a1d01f2f8beb3aa8ebcf1d581c5
ArthurMatosCoder <arthurmatos.studiogames@gmail.com>
2026-04-12 10:37:24 -0300

    Add rain check functionality in atividade_if_chuva.py

---
+++ atividade_if_chuva.py
@@ -0,0 +1,11 @@
+import random
+
+chuvendo = random.randint(1, 2)
+# isso diz que esta chuvendo ou não (1 igual a chuva e 2 igual a sem chuva)
+
+print (chuvendo)
+
+if chuvendo == 1:
+    print ("esta chuvendo, pegue o guarda-chuva")
+if chuvendo == 2:
+    print ("não esta chuvendo, pode ir")
