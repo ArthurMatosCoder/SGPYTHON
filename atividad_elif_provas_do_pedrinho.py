d8afc1c1481dbbf4aa1c6ac237f2447b8ab316cf
ArthurMatosCoder <arthurmatos.studiogames@gmail.com>
2026-05-24 10:35:59 -0300

    Change input types to float and update messages

--- atividad_elif_provas_do_pedrinho.py
+++ atividad_elif_provas_do_pedrinho.py
@@ -1,16 +1,25 @@
-prova1 = int(input("qual a primeira nota? : "))
+prova1 = float(input("qual a primeira nota? : "))
 
-prova2 = int(input("qual a segunda nota? : "))
+prova2 = float(input("qual a segunda nota? : "))
 
-prova3 = int(input("qual a terceira nota? : "))
+prova3 = float(input("qual a terceira nota? : "))
 
 media = (prova1 + prova2 + prova3) / 3
 
 print(f"sua media foi {media}")
 
-if media < 5 :
-    print ("ce foi um lixo mano")
+if media <= 4.999 :
+    print ("CE FOI UM LIXO E REPROVOU COM UM F")
+elif media <= 5.999 :
+    print ("ce foi um lixo mano tirou um F mas não reprovou MELHORE")
 elif media < 10 :
-    print("ce foi aprovado :D")
+    if media <= 6.999 :
+        print("ce foi aprovado com um D")
+    elif media <= 7.999 :
+        print("ce foi aprovado com um C")
+    elif media <= 8.999 :
+        print("ce foi aprovado com um B")
+    elif media < 10 :
+        print("ce foi aprovado com um A")
 else:
-    print("CE E LOCO PERFCT")
+    print("CE E LOCO PERFCT com um A")
