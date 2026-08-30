50588b8f9fcf6b3cf2106a1dfe8d1842044c3030
ArthurMatosCoder <arthurmatos.studiogames@gmail.com>
2026-08-30 12:16:41 -0300

    Refactor fruit list and add user data handling
    Updated the order of fruits, modified the number list, and added new print statements for the person and user dictionaries. Included list comprehensions for squares.

--- while_for_continue.py
+++ while_for_continue.py
@@ -23,14 +23,116 @@ for numero in range(5):
 
 
 
-frutas = ["maçã", "banana", "uva"]
+
+frutas = ["uva", "banana", "maçã"]
+numero = [10, 20, 15, 45, 5]
 frutas.append("maracuja")
+numero.remove(20)
+frutas.remove("banana")
 frutas.remove(frutas[0])
 naoquero = frutas.pop(1)
 frutas.insert(0, 50)
 del frutas[1]
 for fruta in frutas:
     print(fruta)
+    print(numero)
 
-custo = frutas[0] + 50
-print(custo)
+numero.sort()
+print(numero)
+frutas.sort()
+print(frutas)
+numero.reverse()
+print(numero)
+frutas.reverse()
+print(frutas)
+
+
+
+
+
+
+
+
+
+
+pessoa = {
+    'nome': 'ana',
+    'idade': 30,
+    'cidade': 'fortaleza',
+    'cidade': 'fortaleza'}
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(pessoa['cidade'])
+print(pessoa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+usuario = {
+    "nome": "Carlos",
+    "idade": 28,
+    "cidade": "Santos",
+    "profissão": "professor"
+}
+
+for vaca, boi in usuario.items():
+    print(vaca, boi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+quadrados = [x ** 2 for x in range(5)]
+print(quadrados)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
