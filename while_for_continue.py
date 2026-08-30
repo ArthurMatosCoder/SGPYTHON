9b485334133aed08a0435209a682e1bbc147e876
ArthurMatosCoder <arthurmatos.studiogames@gmail.com>
2026-08-30 10:48:48 -0300

    Modify fruit list and print operations

--- while_for_continue.py
+++ while_for_continue.py
@@ -17,3 +17,20 @@ for numero in range(5):
     if numero == 2:
         continue
     print(numero)
+
+
+
+
+
+
+frutas = ["maçã", "banana", "uva"]
+frutas.append("maracuja")
+frutas.remove(frutas[0])
+naoquero = frutas.pop(1)
+frutas.insert(0, 50)
+del frutas[1]
+for fruta in frutas:
+    print(fruta)
+
+custo = frutas[0] + 50
+print(custo)
